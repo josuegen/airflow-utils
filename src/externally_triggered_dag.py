3c857258d680533aa616bac8281d26beb99bf3fc
Josué Velázquez Gen <jvelazquezg6@gmail.com>
2024-11-26 12:32:01 -0600

    Add files via upload

---
+++ src/externally_triggered_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+default_args = {
+    'owner': 'CVS',
+    'depends_on_past': True,
+    'email': ['josuegen@google.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 0,
+}
+
+dag = DAG(
+    dag_id='externally_triggered_dag',
+    start_date=None,
+    schedule=None,
+    default_args=default_args,
+    catchup=False,
+    tags=["external", "triggered"]
+)
+
+some_task = BashOperator(
+    task_id="some_task",
+    bash_command="echo \"here is the conf passed: '$conf'\"",
+    env={"conf": '{{ dag_run.conf if dag_run.conf else "" }}'},
+    dag=dag
+)
+
+some_other_task = BashOperator(
+    task_id="some_other_task",
+    bash_command="sleep 60",
+    dag=dag
+)
+
+some_task >> some_other_task
