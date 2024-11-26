a5d78718e92cbd24eb3b3e38622b3cf0d7c5af70
Josué Velázquez Gen <jvelazquezg6@gmail.com>
2024-11-26 12:31:33 -0600

    Update dataproc_operator_dag.py

--- src/dataproc_operator_dag.py
+++ src/dataproc_operator_dag.py
@@ -12,7 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Author: josuegen@google.com
+
+"""
+This DAG demonstrates how to:
+    1. Provision a Dataproc cluster
+    2. Submit a Pyspark Job to the Dataproc cluster
+    3. Delete the Datapŕoc cluster upon completion of the Job
+
+Author: josuegen@google.com
+"""
 
 from airflow import DAG
 from airflow.providers.google.cloud.operators.dataproc import (
@@ -23,7 +31,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 
 
 default_args = {
-    'owner': 'CVS',
+    'owner': 'defualt',
     'depends_on_past': False,
     'email': ['josuegen@google.com'],
     'email_on_failure': True,
@@ -37,88 +45,60 @@ dag = DAG(
     schedule=None,
     default_args=default_args,
     catchup=False,
-    tags=["dataproc", "pbm"]
+    tags=["dataproc", "pyspark"]
 )
 
 
-"""
 cluster_dict = {
-    'config_bucket': 'cvs-poc-config-bucket',  # value of --bucket in the bash command
+    'config_bucket': '',  # TODO: Developer set config bucket
     'gce_cluster_config': {
-        'subnetwork_uri': 'projects/josuegen-gsd/regions/us-central1/subnetworks/default',  # value of --subnet in the bash command
-        'service_account': '447100982147-compute@developer.gserviceaccount.com',  # value of --service-account in the bash command
+        'subnetwork_uri': 'projects/<project>/regions/<region>/subnetworks/<subnet>',  # TODO: Developer set project, region and subnet
+        'service_account': '',  # TODO: Developer set service account
         'service_account_scopes': ['https://www.googleapis.com/auth/cloud-platform'],
-        'tags': ['allow-iap', 'dataproc-edp-nonprod-pbmshared-copmpute'],
-        'internal_ip_only': True, # equivalent to --no-address
+        'tags': ['', ''], # TODO: Developer set network tags
+        'internal_ip_only': True, # TODO: Developer set only internal or not traffic
     },
     'initialization_actions': [
-        {'executable_file': 'gs://usm-edp-dev-voltage-lob/init-scripts/voltage-init.sh'},
-        {'executable_file': 'gs://goog-dataproc-initialization-actions-us-east4/cloud-sql-proxy/cloud-sql-proxy.sh'}
+        {'executable_file': 'gs://<bucket>/<prefix>.sh'}, # TODO: Developer set bucket and prefix
+        {'executable_file': 'gs://<bucket>/<prefix>.sh'} # TODO: Developer set bucket and prefix
     ],
     'worker_config': {
-        'num_instances': 2,
-        'machine_type_uri': 'n1-standard-4',
+        'num_instances': 2, # TODO: Developer adjust worker count
+        'machine_type_uri': 'n1-standard-4', # TODO: Developer adjust worker machine type
         'disk_config': {
-            'boot_disk_size_gb': 1000
+            'boot_disk_size_gb': 1000 # TODO: Developer adjust worker disk size
         }
     },
     'master_config': {
-        'machine_type_uri': 'n1-standard-4',
+        'machine_type_uri': 'n1-standard-4', # TODO: Developer adjust master machine type
         'disk_config': {
-            'boot_disk_size_gb': 1000
+            'boot_disk_size_gb': 1000 # TODO: Developer adjust master disk size
         }
     },
     'software_config': {
-        'image_version': '2.0.113-debian10'
+        'image_version': '2.0.113-debian10' # TODO: Developer adjust Dataproc image
     },
     'encryption_config': {
-        'gce_pd_kms_key_name': 'projects/cvs-key-vault-nonprod/locations/us-east4/keyRings/gkr-nonprod-us-eats4/cryptoKeys/gk-edp-dev-pbmedh-orch-us-east4'
+        'gce_pd_kms_key_name': 'projects/cvs-key-vault-nonprod/locations/us-east4/keyRings/gkr-nonprod-us-eats4/cryptoKeys/gk-edp-dev-pbmedh-orch-us-east4' # TODO: Developer adjust KMS key (if any, only for CMEK)
     },
     'autoscaling_config': {
-        'policy_uri': 'projects/[project_id]/locations/[dataproc_region]/autoscalingPolicies/us-f4zou1-policy'
+        'policy_uri': 'projects/<project_id>/locations/<dataproc_region>/autoscalingPolicies/<policy_id>' # TODO: Developer adjust autoscaling policy (if any)
     }
 }
-"""
 
-cluster_dict = {
-    'config_bucket': 'cvs-poc-config-bucket',  # value of --bucket in the bash command
-    'gce_cluster_config': {
-        'subnetwork_uri': 'projects/josuegen-gsd/regions/us-central1/subnetworks/default',  # value of --subnet in the bash command
-        'service_account': '447100982147-compute@developer.gserviceaccount.com',  # value of --service-account in the bash command
-        'service_account_scopes': ['https://www.googleapis.com/auth/cloud-platform'],
-        'tags': ['allow-iap', 'dataproc-edp-nonprod-pbmshared-copmpute'],
-        'internal_ip_only': True,
-    },
-    'worker_config': {
-        'num_instances': 2,
-        'machine_type_uri': 'n1-standard-4',
-        'disk_config': {
-            'boot_disk_size_gb': 1000
-        }
-    },
-    'master_config': {
-        'machine_type_uri': 'n1-standard-4',
-        'disk_config': {
-            'boot_disk_size_gb': 1000
-        }
-    },
-    'software_config': {
-        'image_version': '2.0.113-debian10'
-    }
-}
 
 create_ephemeral_cluster = DataprocCreateClusterOperator(
     dag=dag,
     task_id='create_dataproc_cluster',
-    cluster_name='edp-dev-pbmedh-orch-dataproc-00',
-    project_id='josuegen-gsd',  # value of --project in the bash command
+    cluster_name='dataproc-00', # TODO: Developer adjust cluster name
+    project_id='josuegen-gsd',  # TODO: Developer adjust cluster project name
     use_if_exists=True,
     delete_on_error=True,
     cluster_config=cluster_dict,
-    region='us-central1',  # value of --region in the bash command
-    labels={
+    region='us-central1',   # TODO: Developer adjust cluster region
+    labels={ # TODO: Developer adjust cluster labels
         'environment': 'dev',
-        'team': 'edp-pbmedh-data-operations'
+        'team': 'my-team'
     },
     polling_interval_seconds=60,
     deferrable=True
@@ -126,15 +106,15 @@ create_ephemeral_cluster = DataprocCreateClusterOperator(
 
 job_dict = {
     'reference': {
-        'project_id': 'josuegen-gsd',
-        'job_id': 'composerjob1230'
+        'project_id': 'josuegen-gsd', # TODO: Developer adjust project ID for the job
+        'job_id': 'composerjob1230' # TODO: Developer set Job ID to the PySpark job
     },
     'placement': {
-        'cluster_name': 'edp-dev-pbmedh-orch-dataproc-00'
+        'cluster_name': 'dataproc-00' # TODO: Developer set the cluster used to place the Job (line 93)
     },
     'pyspark_job': {
-        'main_python_file_uri': 'gs://cvs_poc_source_bucket/spark_demo.py',
-        'args': ['--one_argument', 'test1', '--other_argument', 'test2']
+        'main_python_file_uri': 'gs://poc_source_bucket/spark_demo.py', # TODO: Developer set PySpark main file URI in GCS
+        'args': ['--one_argument', 'test1', '--other_argument', 'test2'] #  TODO: Developer set the arguments to be passed to the main PySpark job
     },
     'labels': {
         'key': 'value'
@@ -144,18 +124,18 @@ job_dict = {
 submit_dataproc_job = DataprocSubmitJobOperator(
     dag=dag,
     task_id='submit_job_to_cluster',
-    project_id='josuegen-gsd',
+    project_id='josuegen-gsd', # TODO: Developer to adjust the Spark job project
     asynchronous=False,
     job=job_dict,
-    region='us-central1'
+    region='us-central1' # TODO: Developer to adjust the Spark job region
 )
 
 delete_cluster = DataprocDeleteClusterOperator(
     dag=dag,
     task_id='delete_cluster',
-    project_id='josuegen-gsd',
-    region='us-central1',
-    cluster_name='edp-dev-pbmedh-orch-dataproc-00',
+    project_id='josuegen-gsd', # TODO: Developer to adjust the Dataproc cluster project
+    region='us-central1', # TODO: Developer to adjust the Dataproc cluster region
+    cluster_name='dataproc-00', # TODO: Developer to adjust the Dataproc cluster ID
     deferrable=True,
     polling_interval_seconds=30,
     trigger_rule='all_done',
