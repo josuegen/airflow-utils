af0077d5836971563502e989506556fcfb118fee
Josué Velázquez Gen <jvelazquezg6@gmail.com>
2025-03-10 13:09:28 -0600

    Update http_to_gcs.py

--- src/http_to_gcs.py
+++ src/http_to_gcs.py
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""This module contains operator to move data from HTTP endpoint to S3."""
+"""This module contains operator to move data from HTTP endpoint to GCS."""
 
 from __future__ import annotations
 
